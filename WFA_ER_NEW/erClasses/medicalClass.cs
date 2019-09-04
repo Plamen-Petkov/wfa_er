cc4c3700a2e7aa31c1d93b4d07e157486e136937
Pamen-Petkov <plamkata75@gmail.com>
2019-09-04 13:56:50 +0300

    Add Method Update and Delete

--- WFA_ER_NEW/erClasses/medicalClass.cs
+++ WFA_ER_NEW/erClasses/medicalClass.cs
@@ -108,5 +108,82 @@ namespace WFA_ER_NEW.erClasses
             return isSuccess;
         }
 
+        // Update data
+        public bool Update(medicalClass m)
+        {
+            bool isSuccess = false;
+            SqlConnection conn = new SqlConnection();
+            try
+            {
+                string sql = "UPDATE ambulance SET pin=@pin , FirstName=@FirstName, LastName=@LastName, DataTime=@DataTime, mkb=@mkb, KLabKK=@KLabKK, KLabOther=@KLabOther, KLabUrina=@KLabUrina, Rentgen=@Rentgen, UZD=@UZD, KAT=@KAT, suture=@suture, foreignMatter=@foreignMatter, bandaging=@bandaging, Immobilization=@Immobilization, other=@other";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@pin", m.pin);
+                cmd.Parameters.AddWithValue("@FirstName", m.FirstName);
+                cmd.Parameters.AddWithValue("@LastName", m.LastName);
+                cmd.Parameters.AddWithValue("@DataTime", m.DataTime);
+                cmd.Parameters.AddWithValue("@mkb", m.mkb);
+                cmd.Parameters.AddWithValue("@KLabKK", KLabKK);
+                cmd.Parameters.AddWithValue("@KLabOther", m.KLabOther);
+                cmd.Parameters.AddWithValue("@KLabUrina", m.KLabUrina);
+                cmd.Parameters.AddWithValue("@Rentgen", m.Rentgen);
+                cmd.Parameters.AddWithValue("@UZD", m.UZD);
+                cmd.Parameters.AddWithValue("@KAT", m.KAT);
+                cmd.Parameters.AddWithValue("@suture", m.suture);
+                cmd.Parameters.AddWithValue("@foreignMatter", m.foreignMatter);
+                cmd.Parameters.AddWithValue("@bandaging", m.bandaging);
+                cmd.Parameters.AddWithValue("@Immobilization", m.Immobilization);
+                cmd.Parameters.AddWithValue("@other", m.other);
+                conn.Open();
+                int rows = cmd.ExecuteNonQuery();
+                if (rows > 0)
+                {
+                    isSuccess = true;
+                }
+                else
+                {
+                    isSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return isSuccess;
+        }
+        //Method Delete
+        public bool Delete(medicalClass m)
+        {
+            bool isSuccess = false;
+            SqlConnection conn = new SqlConnection();
+            try
+            {
+                string sql = "DELETE FROM ambilance WHERE ambNo=@ambNo";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@ambNo", m.ambNo);
+                conn.Open();
+                int rows = cmd.ExecuteNonQuery();
+                if (rows > 0)
+                {
+                    isSuccess = true;
+                }
+                else
+                {
+                    isSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+               
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
     }
 }
